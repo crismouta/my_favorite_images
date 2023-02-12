b41ada32a769ee3a6be6e158ecf0abb3f5603c24
crismouta <82060703+crismouta@users.noreply.github.com>
2023-02-12 10:55:24 +0100

    adding images seeders

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use App\Models\User;
+use App\Models\MyImage;
 
 class DatabaseSeeder extends Seeder
 {
@@ -15,6 +16,77 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        User::factory()->create(['name' => 'user', 'email' => 'user@user.com']);
+        User::factory()->create(['name' => 'user1', 'email' => 'user1@user.com']);
+        User::factory()->create(['name' => 'user2', 'email' => 'user2@user.com']);
+
+        MyImage::factory()->create([
+            'title' => 'Autumn',
+            'description' => 'Lorem ipsum is placeholder text commonly used in the graphic, print, and publishing industries for previewing layouts and visual mockups.',
+            'image' => 'img/autumn.jpg',            
+            'user_id' => '1',
+        ]);
+
+        MyImage::factory()->create([
+            'title' => 'Autumn',
+            'description' => 'Lorem ipsum is placeholder text commonly used in the graphic, print, and publishing industries for previewing layouts and visual mockups.',
+            'image' => 'img/autumn.jpg',            
+            'user_id' => '2',
+        ]);
+
+        MyImage::factory()->create([
+            'title' => 'Grass',
+            'description' => 'Lorem ipsum is placeholder text commonly used in the graphic, print, and publishing industries for previewing layouts and visual mockups.',
+            'image' => 'img/grass.jpg',            
+            'user_id' => '1',
+        ]);
+
+        MyImage::factory()->create([
+            'title' => 'Grass',
+            'description' => 'Lorem ipsum is placeholder text commonly used in the graphic, print, and publishing industries for previewing layouts and visual mockups.',
+            'image' => 'img/grass.jpg',            
+            'user_id' => '2',
+        ]);
+
+        MyImage::factory()->create([
+            'title' => 'Mountain',
+            'description' => 'Lorem ipsum is placeholder text commonly used in the graphic, print, and publishing industries for previewing layouts and visual mockups.',
+            'image' => 'img/mountain.jpg',            
+            'user_id' => '1',
+        ]);
+
+        MyImage::factory()->create([
+            'title' => 'Mountain',
+            'description' => 'Lorem ipsum is placeholder text commonly used in the graphic, print, and publishing industries for previewing layouts and visual mockups.',
+            'image' => 'img/mountain.jpg',            
+            'user_id' => '2',
+        ]);
+
+        MyImage::factory()->create([
+            'title' => 'River',
+            'description' => 'Lorem ipsum is placeholder text commonly used in the graphic, print, and publishing industries for previewing layouts and visual mockups.',
+            'image' => 'img/river.jpg',            
+            'user_id' => '1',
+        ]);
+
+        MyImage::factory()->create([
+            'title' => 'River',
+            'description' => 'Lorem ipsum is placeholder text commonly used in the graphic, print, and publishing industries for previewing layouts and visual mockups.',
+            'image' => 'img/river.jpg',            
+            'user_id' => '2',
+        ]);
+
+        MyImage::factory()->create([
+            'title' => 'Winter',
+            'description' => 'Lorem ipsum is placeholder text commonly used in the graphic, print, and publishing industries for previewing layouts and visual mockups.',
+            'image' => 'img/winter.jpg',            
+            'user_id' => '1',
+        ]);
+
+        MyImage::factory()->create([
+            'title' => 'Winter',
+            'description' => 'Lorem ipsum is placeholder text commonly used in the graphic, print, and publishing industries for previewing layouts and visual mockups.',
+            'image' => 'img/winter.jpg',            
+            'user_id' => '2',
+        ]);
     }
 }
